0ff4315a69376424623fa5e8bc1784004784afe8
Minh Ly <minh.ly@doforms.com>
2020-07-03 00:37:21 -0400

    Section2: Complete

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription} from 'rxjs';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
@@ -7,7 +8,7 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy{
 
   // posts = [
   //   {title: "First Post", content: "Content"},
@@ -15,8 +16,21 @@ export class PostListComponent {
   //   {title: "Third Post", content: "Content"},
   // ]
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postSub: Subscription;
 
-  constructor(public postService: PostsService);
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(){
+    this.postSub.unsubscribe();
+  }
 
 }
