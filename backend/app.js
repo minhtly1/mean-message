676cfa8330dba0dc3f61148d5cc1411d357f8c6b
Minh Ly <minhtly1@gmail.com>
2020-07-06 00:12:54 -0400

    MongoDB - POST and Fetch from MongoDB cluster

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 
+mongoose.connect("mongodb+srv://minh:Deadflame1@cluster0.eej36.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+    console.log('Connected to database!');
+})
+.catch(() => {
+    console.log('Connection failed!')
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -15,29 +24,23 @@ app.use((req, res, next) => {
 })
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body; 
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    })
     console.log(post);
+    post.save();
     res.status(201).json({
         message: 'Post added successfully'
     });
 });
 
 app.get("/api/posts", (req, res, next) => {
-    const posts = [
-        {
-            id: "fad12345",
-            title: "First server side post",
-            content: "This is coming from server"
-        }, 
-        {
-            id: "fad12346",
-            title: "Second server side post",
-            content: "This is coming from server"
-        }
-    ];
-    res.status(200).json({
-        message: 'Posts fetched succesfully!',
-        posts: posts
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched succesfully!',
+            posts: documents
+        });
     });
 })
 
