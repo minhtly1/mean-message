1433561058c844baca527e2b98dc2e5503a823fb
Minh Ly <minh.ly@doforms.com>
2020-07-06 21:27:40 -0400

    Finished- POST/Delete from Mongo

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -32,13 +32,23 @@ export class PostsService {
         return this.postsUpdated.asObservable();
     }
 
-    addPost(title: string, content: string){
-        const post: Post = {id: null, title: title, content: content};
-        this.http.post<{message: string}>("http://localhost:3000/api/posts", post)
-        .subscribe(responseData => {
-            console.log(responseData.message);
-            this.posts.push(post);
-            this.postsUpdated.next([...this.posts]);
-        });
-    }
+    addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.http.post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post)
+      .subscribe(responseData => {
+        const id = responseData.postId;
+        post.id = id;
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+    deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 }
